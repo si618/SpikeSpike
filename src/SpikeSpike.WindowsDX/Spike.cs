df3f6919d2d49b986fdadfeecddaea167f5b4cde
si <sshnug.si+github@gmail.com>
2016-02-28 15:42:24 +1030

    Finish converting Santa to Spike class. Needs testing!

--- src/SpikeSpike.WindowsDX/Spike.cs
+++ src/SpikeSpike.WindowsDX/Spike.cs
@@ -1,20 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 // Shared amongst projects
 // ReSharper disable once CheckNamespace
+
 namespace SpikeSpike
 {
     public class Spike
     {
-        public int SpikeX { get; } = 50;
-        public int SpikeWidth { get; }
+        private const float Gravity = 0.02f;
+
+        public Spike(SpriteTexture spriteTexture, Keys trigger, int startBottom)
+        {
+            SpriteTexture = spriteTexture;
+            SpikeHeight = spriteTexture.Texture.Height;
+            SpikeWidth = spriteTexture.SpriteWidth;
+            Trigger = trigger;
+            Y = startBottom - SpikeHeight;
+            DY = 0.0f;
+            SpriteTimer = 0.0f;
+            SpriteIndex = 0;
+        }
+
+        public SpriteTexture SpriteTexture { get; }
         public int SpikeHeight { get; }
+        public int SpikeWidth { get; }
+        public int SpikeX { get; } = 50;
         public Keys Trigger { get; }
 
         public float Y { get; set; }
@@ -24,83 +38,68 @@ namespace SpikeSpike
         public int SpriteIndex { get; set; }
         public bool IsJumping { get; set; }
 
-        public Spike(SpriteTexture spriteTexture, Keys trigger, int startBottom)
-        {
-            SpikeWidth = spriteTexture.SpriteWidth;
-            SpikeHeight = spriteTexture.Texture.Height;
-            Trigger = trigger;
+        public Rectangle Bounds => new Rectangle(SpikeX, (int)Y, SpikeWidth, SpikeHeight);
 
-            Y = startBottom - SpikeHeight;
-            DY = 0.0f;
-            IsJumping = false;
-            SpriteTimer = 0.0f;
-            SpriteIndex = 0;
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            var spriteBounds = new Rectangle(SpriteIndex * SpriteTexture.SpriteWidth, 0,
+                SpriteTexture.SpriteWidth, SpriteTexture.Texture.Height);
+            spriteBatch.Draw(SpriteTexture.Texture, Bounds, spriteBounds, Color.White);
         }
 
-        public Rectangle Bounds => new Rectangle(SpikeX, (int) Y, SpikeWidth, SpikeHeight);
-
-        /*
-            type Santa(spriteTexture : SpriteTexture, trigger, startBottom) =
-
-            let mutable y = single(startBottom - santaHeight)
-            let mutable dy = 0.0f
-            let mutable isJumping = false
-            let mutable spriteTimer = 0.0f
-            let mutable spriteIndex = 0
-
-            member this.Bounds
-                with get() = Rectangle(santaX, int(y), santaWidth, santaHeight)
-
-            member this.Update(deltaTime, isKeyPressedSinceLastFrame : Keys -> bool, trackBounds : Rectangle) =
-                // Should we start a jump?
-                if not isJumping && isKeyPressedSinceLastFrame(trigger) then
-                    isJumping <- true
-                    dy <- -0.45f
-
-                if isJumping then
-                    // Physics!
-                    y <- y + dy * deltaTime
-
-                    let hasLanded = int(y) + santaHeight >= trackBounds.Bottom
-                    if hasLanded then
-                        y<- single(trackBounds.Bottom - santaHeight)
-                        isJumping<- false
-                        dy<- 0.0f
-                    else
-                        dy<- dy + gravity
-                else
-                    // Update sprite.
-                    let spriteChangeTime = 80.0f
-                    spriteTimer<- spriteTimer + deltaTime
-                    if spriteTimer >= spriteChangeTime then
-                        spriteTimer<- 0.0f
-                        let wrap value max =
-                            if value> max then 0 else value
-                        spriteIndex<- wrap (spriteIndex + 1) (spriteTexture.numSprites - 1)
-
-            member this.Draw(spriteBatch : SpriteBatch) =
-                let spriteBounds =
-                    Rectangle(spriteIndex * spriteTexture.spriteWidth, 0,
-                              spriteTexture.spriteWidth, 
-                              spriteTexture.texture.Height)
-                spriteBatch.Draw(spriteTexture.texture, this.Bounds, 
-                                 System.Nullable(spriteBounds),
-                                 Color.White)
-
-            // Used for pixel-perfect collision detection.
-            member this.AnyNonTransparentPixels(x1, x2, y1, y2) =
-                let xOffset = spriteTexture.spriteWidth * spriteIndex
-                let pixelsInsideRegion = seq
+        // Used for pixel-perfect collision detection.
+        public bool AnyNonTransparentPixels(int x1, int x2, int y1, int y2)
+        {
+            var xOffset = SpriteTexture.SpriteWidth * SpriteIndex;
+            var pixelsInsideRegion = new List<Color>();
+            for (var y = y1; y < y2 - 1; y++)
+            {
+                for (var x = x1; (x + xOffset) < (x2 + xOffset - 1); x++)
                 {
-                    for y in y1..y2 - 1 do
-                            for x in (x1 + xOffset)..(x2 + xOffset - 1) do
-                                    let index = (y * spriteTexture.texture.Width) + x
-                                yield spriteTexture.textureData.[index]
-                }
-                Seq.exists (fun c -> c <> Color.Transparent) pixelsInsideRegion
-
+                    var index = (y * SpriteTexture.Texture.Width) + x;
+                    pixelsInsideRegion.Add(SpriteTexture.TextureData[index]);
                 }
             }
-        */
+            return pixelsInsideRegion.Any(c => c != Color.Transparent);
+        }
+
+        public void Update(float deltaTime, Keys isKeyPressedSinceLastFrame, Rectangle trackBounds)
+        {
+            // Should we start a jump?
+            if (!IsJumping && Trigger.HasFlag(isKeyPressedSinceLastFrame))
+            {
+                IsJumping = true;
+                DY = -0.45f;
+            }
+
+            if (IsJumping)
+            {
+                // Physics!
+                Y = -Y + DY * deltaTime;
+
+                var hasLanded = (int)Y + SpikeHeight >= trackBounds.Bottom;
+                if (hasLanded)
+                {
+                    Y = trackBounds.Bottom - SpikeHeight;
+                    IsJumping = false;
+                    DY = 0.0f;
+                }
+                else
+                {
+                    DY = -DY + Gravity;
+                }
+            }
+            else
+            {
+                // Update sprite.
+                SpriteTimer = SpriteTimer + deltaTime;
+                const float spriteChangeTime = 80.0f;
+                if (SpriteTimer < spriteChangeTime) return;
+                SpriteTimer = -0.0f;
+                var value = SpriteIndex + 1;
+                var max = SpriteTexture.NumSprites - 1;
+                SpriteIndex = value < max ? value : 0;
+            }
+        }
     }
 }
